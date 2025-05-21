169d1e77aeee965542e32a3112f176dd40c3426a
lirianecastro <lirianecastroacservicos@gmail.com>
2025-05-21 15:26:48 -0300

    tarefa 2

--- controller/usuarioController.py
+++ controller/usuarioController.py
@@ -7,11 +7,7 @@ from model.usuario import *
 
 Sesssiolocal =sessionmaker(autocommit=False,bind=engine)
 
-@app.route("/usuarios", methods=["GET"])
-def usuarios():
-    db = Sesssiolocal()
-    usuarios = db.query(usuario).all()
-    return jsonify([usuario.to_dict() for usuario in usuarios]), 200
+
 
 @app.route("/usuarios/novo", methods=["GET"])
 def novo ():
@@ -25,3 +21,54 @@ def create():
     db.add(uso)
     db.commit()
     return jsonify({'msg':'salvo com sucesso'}), 200
+
+@app.route("/usuarios", methods=["GET"])
+def usuarios():
+    db = Sesssiolocal()
+    usuarios = db.query(usuario).all()
+    return jsonify([usuario.to_dict() for usuario in usuarios]), 200
+
+@app.route("/usuarios/<int:id>", methods =["GET"])
+def get_usuarios(id):
+    db = Sesssiolocal()
+    usu = db.query(usuario).get(id)
+    if(usu):
+        return jsonify(usu.to_dict()), 200
+    else:
+        return jsonify({'msg': 'usuario não encontrado'}), 404
+
+@app.route("/usuarios/<int:id>", methods =["DELETE"])
+def delete_usuarios(id):
+    db = Sesssiolocal()
+    usu = db.query(usuario).get(id)
+    if(usu):
+       db.delete(usu)
+       db.commit()
+       return jsonify({'usuario apagado'}), 204
+    else:
+        return jsonify({'msg': 'usuario não encontrado'}), 404
+
+@app.route("/usuarios", methods =["POST"])
+def creat_usuarios():
+    db = Sesssiolocal()
+    data = request.get_json()
+    usu = usuario(nome=data['nome'], data=data['aniversario'])
+    db.add(usu)
+    db.commit()
+    return jsonify({'msg':'usuario criado com sucesso!',
+                     'usu':usu.to_dict()}), 201
+
+
+
+@app.route("/usuarios/<int:id>", methods =["PUT"])
+def update_usuarios(id):
+    db = Sesssiolocal()
+    ub = db.query(usuario).get(id)
+    data= request.get_json()
+    ub.nome = data['nome']
+    ub.data = data['aniversario']
+
+    db.commit()
+    return jsonify({'msg':'usuario atualizado com sucesso!',
+                     'usu':ub.to_dict()}), 200
+
